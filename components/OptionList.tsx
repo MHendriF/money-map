b4610aef85845738deb5c4a78144dd55e656e8fb
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-14 10:11:00 +0700

    add option list component

---
+++ components/OptionList.tsx
@@ -0,0 +1,33 @@
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
+import { Currencies, Currency } from "@/lib/currencies";
+
+export default function OptionList({
+  setOpen,
+  setSelectedOption,
+}: {
+  setOpen: (open: boolean) => void;
+  setSelectedOption: (status: Currency | null) => void;
+}) {
+  return (
+    <Command>
+      <CommandInput placeholder="Filter currency..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup>
+          {Currencies.map((currency: Currency) => (
+            <CommandItem
+              key={currency.value}
+              value={currency.value}
+              onSelect={(value) => {
+                setSelectedOption(Currencies.find((priority) => priority.value === value) || null);
+                setOpen(false);
+              }}
+            >
+              {currency.label}
+            </CommandItem>
+          ))}
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  );
+}
