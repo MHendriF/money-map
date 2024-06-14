8d08ee8864c0fda88c45914b57c46af407612611
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-14 13:38:41 +0700

    add &rsquo;

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -60,8 +60,8 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
                 Your ultimate personal finance companion. MoneyMap helps you track your income, expenses, and savings
                 effortlessly. With an intuitive interface and powerful features, you can visualize your financial
                 journey, set budgeting goals, and make informed decisions to stay on top of your finances. Whether
-                you're planning for a big purchase or simply want to manage daily expenses, MoneyMap guides you every
-                step of the way. Navigate your financial future with confidence and ease.
+                you&rsquo;re planning for a big purchase or simply want to manage daily expenses, MoneyMap guides you
+                every step of the way. Navigate your financial future with confidence and ease.
               </p>
             </div>
 
