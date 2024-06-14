f69633610ee676aa841175de966326207472f3d3
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-14 12:30:42 +0700

    implement components category, history

---
+++ app/(dashboard)/_components/CategoryRow.tsx
@@ -0,0 +1,10 @@
+import { Category } from "@prisma/client";
+
+export default function CategoryRow({ category }: { category: Category }) {
+  return (
+    <div className="flex items-center gap-2">
+      <span role="img">{category.icon}</span>
+      <span>{category.name}</span>
+    </div>
+  );
+}
