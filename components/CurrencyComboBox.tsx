bb4c15a865deb5dfe4ea966d9676399cbbe94024
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-14 10:14:33 +0700

    implement select currency

---
+++ components/CurrencyComboBox.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { useMediaQuery } from "@/hooks/use-media-query";
+import { Button } from "@/components/ui/button";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Currencies, Currency } from "@/lib/currencies";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import SkeletonWrapper from "@/components/SkeletonWrapper";
+import { UpdateUserCurrency } from "@/app/wizard/_actions/userSettings";
+import { UserSettings } from "@prisma/client";
+import { toast } from "sonner";
+import { useCallback, useEffect, useState } from "react";
+import OptionList from "./OptionList";
+
+export default function CurrencyComboBox() {
+  const [open, setOpen] = useState(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  const [selectedOption, setSelectedOption] = useState<Currency | null>(null);
+
+  const userSettings = useQuery<UserSettings>({
+    queryKey: ["userSettings"],
+    queryFn: () => fetch("/api/user-settings").then((res) => res.json()),
+  });
+
+  useEffect(() => {
+    if (!userSettings.data) return;
+    const userCurrency = Currencies.find((currency) => currency.value === userSettings.data.currency);
+    if (userCurrency) setSelectedOption(userCurrency);
+  }, [userSettings.data]);
+
+  const mutation = useMutation({
+    mutationFn: UpdateUserCurrency,
+    onSuccess: (data: UserSettings) => {
+      toast.success(`Currency updated successuflly 🎉`, {
+        id: "update-currency",
+      });
+
+      setSelectedOption(Currencies.find((c) => c.value === data.currency) || null);
+    },
+    onError: (e) => {
+      console.error(e);
+      toast.error("Something went wrong", {
+        id: "update-currency",
+      });
+    },
+  });
+
+  const selectOption = useCallback(
+    (currency: Currency | null) => {
+      if (!currency) {
+        toast.error("Please select a currency");
+        return;
+      }
+
+      toast.loading("Updating currency...", {
+        id: "update-currency",
+      });
+
+      mutation.mutate(currency.value);
+    },
+    [mutation]
+  );
+
+  if (isDesktop) {
+    return (
+      <SkeletonWrapper isLoading={userSettings.isFetching}>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button variant="outline" className="w-full justify-start" disabled={mutation.isPending}>
+              {selectedOption ? <>{selectedOption.label}</> : <>Set currency</>}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[200px] p-0" align="start">
+            <OptionList setOpen={setOpen} setSelectedOption={selectOption} />
+          </PopoverContent>
+        </Popover>
+      </SkeletonWrapper>
+    );
+  }
+
+  return (
+    <SkeletonWrapper isLoading={userSettings.isFetching}>
+      <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerTrigger asChild>
+          <Button variant="outline" className="w-full justify-start" disabled={mutation.isPending}>
+            {selectedOption ? <>{selectedOption.label}</> : <>Set currency</>}
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <div className="mt-4 border-t">
+            <OptionList setOpen={setOpen} setSelectedOption={selectOption} />
+          </div>
+        </DrawerContent>
+      </Drawer>
+    </SkeletonWrapper>
+  );
+}
