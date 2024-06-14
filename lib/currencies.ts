ce887b5839e461fbdd0bb8107d56482b987da7f3
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-14 10:05:53 +0700

    setup libs

---
+++ lib/currencies.ts
@@ -0,0 +1,9 @@
+export const Currencies = [
+  { value: "IDR", label: "Rp Rupiah", locale: "id-ID" },
+  { value: "USD", label: "$ Dollar", locale: "en-US" },
+  { value: "EUR", label: "€ Euro", locale: "de-DE" },
+  { value: "JPY", label: "¥ Yen", locale: "ja-JP" },
+  { value: "GBP", label: "£ Pound", locale: "en-GB" },
+];
+
+export type Currency = (typeof Currencies)[0];
